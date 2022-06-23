329601c017389025737b8535d91ec82732f9fa8a
iPaolaDC <paola.dc08@gmail.com>
2022-06-23 16:03:44 -0500

    Sesion 5
    comentarios a los códigos

--- Sesion-5/Ejercicio-1.py
+++ Sesion-5/Ejercicio-1.py
@@ -2,14 +2,16 @@ import math
 
 
 def area_triangulo(base, altura):
+    # se halla el area del triangulo
     a = round((base * altura) / 2)
     return a
 
 
 def area_circulo(r):
+    # Se halla el area del un circulo
     ac = round(math.pi * (pow(r, 2)), 2)
     return ac
 
-
+# se muestran las dos funciones
 print("El área del triangulo es :", area_triangulo(20, 15))
 print("El área del círculo es :", area_circulo(12))
--- Sesion-5/Ejercicio-3.py
+++ Sesion-5/Ejercicio-3.py
@@ -1,7 +1,11 @@
 def year_bisiesto():
+    # pide el ingreso del año
     year: int = int(input("Ingresa el año: "))
+    # validamos que los números sean mayores que 1
     while year <= 1:
         year: int = int(input("El año no es valido. Vuelve a ingresar el año"))
+
+    # Para que sea bisiesto el año debe cumplir con esta condiciòn
     if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
         print(year, "es un AÑO BISIESTO.(Tiene 366 días) ")
     else:
