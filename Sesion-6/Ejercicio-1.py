4ecf2ec3b3dae4d35169f5c378f1957a621df1f8
iPaolaDC <paola.dc08@gmail.com>
2022-06-29 18:35:13 -0500

    Sesión 6
    Solución ejercicio 1 de la sesión 6

---
+++ Sesion-6/Ejercicio-1.py
@@ -0,0 +1,27 @@
+class Vehiculo:
+
+    def __init__(self, color, ruedas, puertas):
+        self.color = color
+        self.ruedas = ruedas
+        self.puertas = puertas
+
+    def __str__(self):
+        return "color:{}, Ruedas:{}, puertas:{}".format(self.color, self.ruedas, self.puertas)
+
+
+class Coche(Vehiculo):
+    def __init__(self, color, ruedas, puertas, velocidad, cilindrada):
+        super().__init__(color, ruedas, puertas)
+        self.color = color
+        self.ruedas = ruedas
+        self.puertas = puertas
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
+
+    def __str__(self) -> object:
+        return "color:{}, Ruedas:{}, puertas:{}, Km/h:{}, CC:{}".format(self.color, self.ruedas, self.puertas,
+                                                                        self.velocidad, self.cilindrada)
+
+
+objeto = Coche("verde", 4, 5, 60, 200)
+print(objeto)
