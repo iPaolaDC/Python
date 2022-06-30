9eb890055c489ba52225d88a1fef517b7f7e7f27
iPaolaDC <paola.dc08@gmail.com>
2022-06-30 18:11:54 -0500

    sesión 7
    modificación  codigo

--- Sesion-7/Ejercicio-1.py
+++ Sesion-7/Ejercicio-1.py
@@ -2,14 +2,21 @@ import operaciones
 
 
 def calculador():
-    res_suma = operaciones.suma(10, 5)
-    res_resta = operaciones.resta(10, 5)
-    res_multiplicar = operaciones.multiplicar(10, 5)
-    res_dividir = operaciones.dividir(10, 5)
-    print("El resultado de la suma es:", res_suma)
-    print("El resultado de la resta es:", res_resta)
-    print("El resultado de la multiplicación es:", res_multiplicar)
-    print("El resultado de la división es:", res_dividir)
+
+    a = input("Ingresa el primer número:")
+    b = input("ingresa el segundo número:")
+
+    a = int(a)
+    b = int(b)
+
+    res_suma = operaciones.suma(a, b)
+    res_resta = operaciones.resta(a, b)
+    res_multiplicar = operaciones.multiplicar(a, b)
+    res_dividir = operaciones.dividir(a, b)
+    print("El resultado de", a, "+", b, "=", res_suma)
+    print("El resultado de", a, "-", b, "=", res_resta)
+    print("El resultado de", a, "x", b, "=", res_multiplicar)
+    print("El resultado de", a, "/", b, "=", res_dividir)
 
 
 if __name__ == '__main__':
